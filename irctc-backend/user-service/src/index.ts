49389bfa1e38e578578a0776c2df331f9430b3b1
ami-inn <amipk2001@gmail.com>
2026-08-09 18:04:48 -0700

    fixed the issues in the cors middleware

--- irctc-backend/user-service/src/index.ts
+++ irctc-backend/user-service/src/index.ts
@@ -1,15 +1,21 @@
-import express from "express";
 import cookieParser from "cookie-parser";
+import express from "express";
+import helmet from "helmet";
 
-
-
+import errorHandler from "@/middlewares/error.middleware";
 import { env } from "@/utils/env";
 
 const app = express();
 
+app.use(helmet());
+app.use(express.json());
+app.use(cookieParser());
+
 app.get("/", (_req, res) => {
   res.send("Hello Node + TypeScript");
 });
+
+app.use(errorHandler);
 
 app.listen(env.port, () => {
   console.log("Server running on http://localhost:" + env.port);
--- irctc-backend/user-service/src/middlewares/error.middleware.ts
+++ irctc-backend/user-service/src/middlewares/error.middleware.ts
@@ -1,25 +1,24 @@
 
-import { Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 
-import { AppError } from "@/utils/asyncHandler";
+import { AppError } from "@/utils/error";
 
-
-
-module.exports = (err: AppError, _req: Request, res: Response, _next: NextFunction) => {
-    
-    if(err instanceof AppError){
-        return res.status(err.statusCode as number).json({
+const errorHandler = (err: AppError, _req: Request, res: Response, _next: NextFunction) => {
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json({
             success: false,
-            error:err.code,
+            error: err.code,
             message: err.message,
-            stack: err.stack
+            stack: err.stack,
         });
     }
-    console.error('unhandled error',err);
+
+    console.error("unhandled error", err);
     return res.status(500).json({
         success: false,
-        error: 'INTERNAL_SERVER_ERROR',
-        message: 'Internal Server Error',
-        
+        error: "INTERNAL_SERVER_ERROR",
+        message: "Internal Server Error",
     });
 };
+
+export default errorHandler;
